a6dad0b572aae968307726829037aee162cfe2f2
love8279 <lovekushchoudhary315@gmail.com>
2026-03-03 17:06:08 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+API_ID = int(os.environ.get("API_ID", "12345")) # अपना API ID डालें
+API_HASH = os.environ.get("API_HASH", "your_api_hash")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token")
